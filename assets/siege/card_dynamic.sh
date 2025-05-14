298628d9636c6117adf5fa1e44127016a5dfbd51
eliezerraj <eliezerraj@gmail.com>
2025-05-13 23:25:50 -0300

    fix:error msg

--- assets/siege/card_dynamic.sh
+++ assets/siege/card_dynamic.sh
@@ -2,7 +2,7 @@
 
 # variabels
 #export AUTH_TOKEN=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJpc3MiOiJnby1vYXV0aC1sYW1iZGEiLCJ2ZXJzaW9uIjoiMyIsImp3dF9pZCI6IjMxZTJkNDFmLTkwZmUtNGVjMy1hNzk4LWQxM2E2MmJmOTcxNCIsInVzZXJuYW1lIjoiYWRtaW4iLCJzY29wZSI6WyJhZG1pbiJdLCJleHAiOjE3NDY0NDUxOTB9.jDizRiRDA9fralRMaNLP7Cik4An_zLAr0w7FPweRPkLtbrZ9QzmrjTuWOVylP55X66tGAS8wXjln3vieg3iTRk-CHB0Xwez7MJZghsXjhbZHDX2oZty99FtbnnGKlEwfgY0_zPEB1XKM63iNoU_y_TQ8vBS_WA5H4OXHEjEDIYqlorQ-ZifpfLTCYfPmi6pE1_z-7qoFbaKbMnL_2cIGQI3efSRGMr73qqmwdt2JxLuHn4eTJHqd5f0QWymf3kWaQ-uhHD_Vif7d0L1pa_wvPzRgOp-budYpGRCbR6kKyHamK6RErr0qjB1yx3QTd7NeBq8DMg5PUIiTI6j47k9jF3q6eGjcMdL_VPk6fWBOq4IcCHAxBBegNNvO6fGlwb27qe_iFUABO8cq_m4jpoy4SaM6yYoUGpuTcbX6AJd7k-68PJfwqDo73XdMnqMd3gO3OGYVo7vEpUuvIkAOiEWesg2aWJEr7sN0IdQcMozyr_Lbgv1BrN-FGn5zGlQSdOeha5DPg5bhC4JBnj4bRLjoPWtwpBLJyZs7PIHxozsocCXCq91FSBmRYsoX0hNvzCZ0WpWxaa2dOzjMT7Vw5bdjUWb2NRp8d22_WIzpt2SZIjykQWcFzpGvY0AiwykFzYGp34Z4c8k4afDIKDApGF0y0X1LfxtwfdvORqrGL5p3DpQ
-export URL_POST=https://go-global.architecture.caradhras.io/card/cardToken
+export URL_POST=https://go-global-apex.architecture.caradhras.io/card/cardToken
 
 RANDOM_PAN=$((RANDOM % 900 + 100))
 
@@ -10,10 +10,60 @@ PAN=111111000001
 PAN_TMP=$(($PAN+$RANDOM_PAN))
 FINAL_PAN="${PAN_TMP:0:3}"."${PAN_TMP:3:3}"."${PAN_TMP:6:3}"."${PAN_TMP:9:3}"
 
-echo '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}'
+#echo '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}'
 
 # POST request
-curl -s -i -X POST "$URL_POST" \
+STATUS_CODE=$(curl -s -i -X POST "$URL_POST" \
 	--header "Content-Type: application/json" \
 	--header "Authorization: $AUTH_TOKEN" \
-	--data '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}' | grep "^HTTP\/"
+	--data '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}' | grep "^HTTP\/")
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR ====> $STATUS_CODE"
+fi
+
+STATUS_CODE=$(curl -s -i -X POST "$URL_POST" \
+	--header "Content-Type: application/json" \
+	--header "Authorization: $AUTH_TOKEN" \
+	--data '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}' | grep "^HTTP\/")
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X POST "$URL_POST" \
+	--header "Content-Type: application/json" \
+	--header "Authorization: $AUTH_TOKEN" \
+	--data '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}' | grep "^HTTP\/")
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X POST "$URL_POST" \
+	--header "Content-Type: application/json" \
+	--header "Authorization: $AUTH_TOKEN" \
+	--data '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}' | grep "^HTTP\/")
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X POST "$URL_POST" \
+	--header "Content-Type: application/json" \
+	--header "Authorization: $AUTH_TOKEN" \
+	--data '{"card_number": "'$FINAL_PAN'","card_type": "CREDIT"}' | grep "^HTTP\/")
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
--- assets/siege/ledger_dynamic.sh
+++ assets/siege/ledger_dynamic.sh
@@ -2,18 +2,65 @@
 
 # variabels
 #export AUTH_TOKEN=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJpc3MiOiJnby1vYXV0aC1sYW1iZGEiLCJ2ZXJzaW9uIjoiMyIsImp3dF9pZCI6IjkyNWFjMDk2LTc3Y2UtNDYyZC1hMDFiLWZkYjQ5NzZjZGVlNSIsInVzZXJuYW1lIjoiYWRtaW4iLCJzY29wZSI6WyJhZG1pbiJdLCJleHAiOjE3NDY0NTA1MTN9.U2mF4BGg_B87182DTnibBS8GBcmUd-Os0uvJ6JA-R9unIHOkq4sT_er14w9AP1mGo541yg5viFQAPqPdQCSUUHwI5d_mmry6xRqG2bSMDpeEDx8ylFQR7wsSofZNjb5IHaoMty1Rii88fpdFMOZEGtPPNvQiy_E51h_4RfFkH0vQa_oVo9VxIuMYaYfAGUuKnTJdjIe0bvJRaCcTQf8ZkTYezHa7RJr2Vq6fJZytY74CMFJhG02Ru_4tXL88M8S54di84dzls57HGJXEjidu1o0GXezdo81vz8z7XR7OZfwAk02GDYwuC0WLmWerNkdTUKRh8wugUTTbD9-IEnWkIcBzirrLBlu3zSf5ssOlcMOaSFn0WAunCZSUHP45qCmwn0kmniCJF7aBfjzpI5edrPxV_YN1pVBz6DkhW5mD2SS5h8sgpS_f24ayMK-gkT5apQa_8KaQyodZqyMDnBljrX2_mDoRp4zgIqkrfjebnsqyuT6mN_k6xRdsfEOMzQY7d3FVLkCDg2XGGoKf8ZpQ_A8OSE-0zXHho2K0PcNdgkWDEg4ylU10RZNSRHXGMuSNcklBXRsg_CoH8jpcdBxBAvSvljQU5oBrU8NkWscNZBOEQBnl3XnnmbC-QcW6NVnmsEsge2EWAVKw_if4mBeTXET3PTjuZ0yLVB0Xs36FKOY
-export URL_POST=https://go-global-02.architecture.caradhras.io/ledger/movimentStatementPerDate
-export URL_POST2=https://go-global-02.architecture.caradhras.io/ledger/movimentStatement
+export URL_POST=https://go-global-apex.architecture.caradhras.io/ledger/movimentStatementPerDate
+export URL_POST2=https://go-global-apex.architecture.caradhras.io/ledger/movimentStatement
 
 RANDOM_ACC=$((RANDOM % 999 + 1))
 
-echo "'$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07"
+#echo "'$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07"
 
 # POST request
-curl -s -i -X GET ''$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07' \
-	--header "Content-Type: application/json" \
-	--header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+STATUS_CODE=$(curl -s -i -X GET ''$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07' \
+          --header "Content-Type: application/json" \
+          --header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+          )
 
-curl -s -i -X GET ''$URL_POST2'/ACC-'$RANDOM_ACC'' \
-	--header "Content-Type: application/json" \
-	--header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X GET ''$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07' \
+          --header "Content-Type: application/json" \
+          --header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+          )
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X GET ''$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07' \
+          --header "Content-Type: application/json" \
+          --header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+          )
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X GET ''$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07' \
+          --header "Content-Type: application/json" \
+          --header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+          )
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
+
+STATUS_CODE=$(curl -s -i -X GET ''$URL_POST'?account-id=ACC-'$RANDOM_ACC'&date_start=2025-04-07' \
+          --header "Content-Type: application/json" \
+          --header "Authorization: $AUTH_TOKEN" | grep "^HTTP\/"\
+          )
+
+if echo "$STATUS_CODE" | grep -q "200"; then
+  echo "$STATUS_CODE"
+else
+  echo "ERROR =========================> $STATUS_CODE  "
+fi
